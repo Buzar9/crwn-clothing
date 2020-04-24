8457c71b452539afc19185823422da4c872e95fa
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-04-24 19:36:11 +0200

    "Sign Up With Email and Password"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor(props) {
@@ -18,16 +18,46 @@ class App extends React.Component {
         }
     }
 
-    // metoda potrzebna do zakończenia sekcji równocześnie z momentem przestania użytkowania component App
+    // metoda potrzebna do zakończenia sesji równocześnie z momentem przestania użytkowania component App
     unsubscribeFromAuth = null;
 
     componentDidMount() {
         // Dopóki component App jest aktywny, doputy autoryzacja użytkownika jest otwarta i mamy dostęp do informacji o użytkowniku
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            // this.setState({ currentUser: user });
+            // console.log(user);
 
-            console.log(user);
-        })
+            //sprawdza czy obiekt userAuth nie jest nullem
+            if(userAuth) {
+              //  tworzy obiekt referencji z podanych userAuth
+              const userRef = await createUserProfileDocument(userAuth);
+
+              // gdy createUserProfileDocument() skopiuje dane użytkownika z logowania google do naszej bazy danych,
+              //  następnie ustawia użytkownika jako zalogowanego w aplikacji
+              userRef.onSnapshot(snapShot => {
+                  //dopóki nie wywoła się .data() snapShot nie udostępni danych dokumentu
+                  // console.log(snapShot.data());
+
+                  this.setState({
+                      currentUser: {
+                          id: snapShot.id,
+                          ...snapShot.data()
+                      }
+                      //    oczywiście this.setState jest asynchroniczne, więc, aby wykonać setState i console.log jednocześnie trzeba consol.log ująć jako osobną funkcję z osobnym
+                      //    wywołaniem
+                      // }, () => {
+                      //     console.log(this.state)
+                      // }
+                  });
+
+                  console.log(this.state);
+              //    dzięki zapisaniu obiektu w state, możemy nasłuchiwać zmian w tym obiekcie
+              });
+            //  jeżeli jednam userAuth jest nullem, aplikacja ustawia currentUser jako userAuth.
+            } else {
+                this.setState({currentUser: userAuth});
+            }
+        });
     }
 
     componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,41 @@ const config = {
     appId: "1:314812852554:web:9f3d8cb78626e523bd39b3"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+    // jezeli userAuth jest nullem to metoda kończy działanie
+    if(!userAuth) return;
+
+    // znajduje referencje w bazie danych (referencje są obiektem)
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    //pobiera dane dla podanych referencji (pobiera jako obiekt)
+    const snapShot = await userRef.get();
+
+    //sprawdza czy takie dane istnieją
+    if(!snapShot.exists) {
+        //jeżeli nie istnieją to tworzy nowy obiekt do zapisania
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        //próbuje zapisać nowy obiekt jako dokument w bazie danych
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        //    jeżeli się nie uda wywala błąd
+        }catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    //na końcu zawsze zwraca referencje (które od początku są obiektem)
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
