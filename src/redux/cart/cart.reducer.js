c77c43ceb15e407b2d0eb0d1f00e762d767c2b8b
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-04-26 08:42:36 +0200

    'Remove Items At Checkout'

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types'
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -25,11 +25,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 // cartItems: [...state.cartItems, action.payload]
             };
 
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
                 ...state,
                 cartItems: state.cartItems.filter(
-                    cartItem => cartItem.id !== action.payload.id
+                    cartItems => cartItems.id !== action.payload.id
                 )
             };
         default:
